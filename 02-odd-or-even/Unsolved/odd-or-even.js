69159e57cd13d51a4e12814579b9a21b368754da
Jack Ritchie <jwf.ritchie@gmail.com>
2021-08-13 12:06:51 +1000

    solved odd and even algo

--- 02-odd-or-even/Unsolved/odd-or-even.js
+++ 02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,16 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function(num) {};
+var oddOrEven = function(num) {
+  if(num % 2 == 0){
+    return "even"
+  } else {
+    return "odd"
+  }
+};
+
+
+// if a number can be devided by 2 with no remainder it is is even.
+// as modulus returns out the remainder of a divisional operation.
+// if  {value} modulus of 2 returns 0 it is considered even, as there are only odd and even numbers(and primes. 2 is prime.)
+
